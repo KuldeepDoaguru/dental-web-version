bfe68d9f05095901428825d85bae501cd52dc09a
mohitsahu21 <mohitsahu1993@gmail.com>
2024-02-14 21:14:49 +0530

    Add react big calender library for calender.

--- frontend/src/pages/receptionist/AppointmentSection.js
+++ frontend/src/pages/receptionist/AppointmentSection.js
@@ -1,11 +1,105 @@
-import React from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import styled from 'styled-components'
 import Sider from '../../components/receptionist/Sider'
 import Header from '../../components/receptionist/Header'
-import { Link } from 'react-router-dom'
+import { Link, json } from 'react-router-dom'
 import HeaderApp from '../../components/receptionist/Appointment/AppNavbar/HeaderApp'
 
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import moment from 'moment'
+import EditPopup from '../../components/receptionist/Appointment/EditPopup'
+import Popup from '../../components/receptionist/Appointment/Popup'
+import ModifyPopup from '../../components/receptionist/Appointment/ModifyPopup'
+import DeletePopup from '../../components/receptionist/Appointment/DeletePopup'
+
+const localizer = momentLocalizer(moment);
+
+
+    
+
 function AppointmentSection() {
+
+  const clickRef = useRef(null);
+  const [selectedSlot, setSelectedSlot] = useState(null);
+  const [showEditPopup, setShowEditPopup] = useState(false);
+  const [showModifyPopup, setShowModifyPopup] = useState(false);
+  const [showDeletePopup, setShowDeletePopup] = useState(false);
+
+  const [showAddPopup, setShowAddPopup] = useState(false);
+
+  useEffect(() => {
+    /**
+     * What Is This?
+     * This is to prevent a memory leak, in the off chance that you
+     * teardown your interface prior to the timed method being called.
+     */
+    return () => {
+      window.clearTimeout(clickRef?.current)
+    }
+  }, [])
+
+  const events = [{
+    start: new Date(),
+    end : new Date(),
+    title : "Appointment Mohit"
+  },
+    {start: new Date(),
+    end : new Date(),
+    title : "Appointment Dev"
+  },
+  {
+    start: new Date(),
+    end : new Date(),
+    title : "Appointment Mohit"
+  },
+    {start: new Date(),
+    end : new Date(),
+    title : "Appointment Dev"
+  }
+]
+
+
+const handleEventSelect = event => {
+  // Set the selected event and open the modal
+  
+  alert("hello")
+};
+
+const onSelectEvent = useCallback((calEvent) => {
+  /**
+   * Here we are waiting 250 milliseconds (use what you want) prior to firing
+   * our method. Why? Because both 'click' and 'doubleClick'
+   * would fire, in the event of a 'doubleClick'. By doing
+   * this, the 'click' handler is overridden by the 'doubleClick'
+   * action.
+   */
+
+  window.clearTimeout(clickRef?.current)
+  clickRef.current = window.setTimeout(() => {
+    // window.alert(`Selected event: ${calEvent.title}`);
+    setSelectedSlot(calEvent);
+      setShowModifyPopup(true);
+  }, 250)
+}, [])
+
+const onSelectSlot = useCallback((slotInfo) => {
+  /**
+   * Here we are waiting 250 milliseconds (use what you want) prior to firing
+   * our method. Why? Because both 'click' and 'doubleClick'
+   * would fire, in the event of a 'doubleClick'. By doing
+   * this, the 'click' handler is overridden by the 'doubleClick'
+   * action.
+   */
+  window.clearTimeout(clickRef?.current)
+  clickRef.current = window.setTimeout(() => {
+    setSelectedSlot(slotInfo);
+      setShowAddPopup(true);
+  }, 250)
+  
+}, [])
+
+
   return (
     <Wrapper>
       <Header/>
@@ -15,8 +109,19 @@ function AppointmentSection() {
    </div>
    <div className="col-lg-11">
     <div className="row">
-      <div className="col-lg-9">
-         <HeaderApp/>
+      <div className="col-lg-10">
+         {/* <HeaderApp/> */}
+
+         <Calendar
+         localizer={localizer}
+         events={events}
+         defaultView='day'
+         onSelectEvent={onSelectEvent}
+         onSelectSlot={onSelectSlot} 
+         selectable
+        
+         />
+
       </div>
   
    
@@ -100,6 +205,42 @@ function AppointmentSection() {
           </div>
    </div>
    </div>
+   {showModifyPopup && (
+        <ModifyPopup onEdit={() => {
+          setShowModifyPopup(false)
+          setShowEditPopup(true)
+          }
+          
+        }
+        onDelete={() => {
+          setShowModifyPopup(false)
+          setShowDeletePopup(true)
+          }
+          
+        }
+        onClose={() => {
+          setShowModifyPopup(false)
+          
+          }
+          
+        }
+        
+        slotInfo={selectedSlot} />
+      )}
+   {showAddPopup && (
+        <Popup onClose={() => setShowAddPopup(false)} slotInfo={selectedSlot} />
+      )}
+
+{showEditPopup && (
+        <EditPopup onClose={() => setShowEditPopup(false)} slotInfo={selectedSlot} />
+      )}
+{showDeletePopup && (
+        <DeletePopup onClose={() => setShowDeletePopup(false)} slotInfo={selectedSlot} />
+      )}
+
+
+
+   
     </Wrapper>
   )
 }
@@ -159,3 +300,6 @@ const Wrapper = styled.div `
     }
   }
 `
+
+
+

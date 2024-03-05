7064b162847dc6480d84e23ecefd7ff0aa9a8d88
mohitsahu21 <mohitsahu1993@gmail.com>
2024-03-05 21:09:31 +0530

    setup the redux toolkit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,15 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import {persistor, store} from './redux/store.js'
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-   
-  </React.StrictMode>
+  <BrowserRouter>
+    
+      <Provider store={store}>
+        <PersistGate persistor={persistor} loading={null}>
+      <App />
+      </PersistGate>
+      </Provider>
+    
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function

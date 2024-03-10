fe3d6562eb6619afa605d77d2ce8438b0d4a0d80
mohitsahu21 <mohitsahu1993@gmail.com>
2024-03-10 21:02:05 +0530

    edit page for patient details.

---
+++ frontend/src/components/receptionist/AllPatients/EditPatientDetails.js
@@ -0,0 +1,348 @@
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import axios from 'axios';
+import Select from 'react-select';
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleTableRefresh } from '../../../redux/user/userSlice';
+import CreatableSelect from 'react-select/creatable';
+
+function EditPatientDetails({ onClose, patientInfo, allAppointmentData }) {
+  const dispatch = useDispatch();
+  const {currentUser} = useSelector((state) => state.user);
+
+  const [show, setShow] = useState(false);
+  const [selectedDisease, setSelectedDisease] = useState([]);
+  const [inputDisease, setInputDisease] = useState('');
+  const [disease, setDisease] = useState([]);
+  
+  const [data, setData] = useState({
+    patientId : patientInfo.uhid,
+    patient_Name: patientInfo.patient_name,
+    mobile: patientInfo.mobileno,
+    email:patientInfo.emailid ,
+    gender: patientInfo.gender,
+    aadhaar_no: patientInfo.aadhaar_no,
+    contact_Person:patientInfo.contact_person,
+    contact_Person_Name:patientInfo.contact_person_name ,
+    blood_Group: patientInfo.bloodgroup,
+    dob: patientInfo.dob,
+    age:patientInfo.age,
+    weight:patientInfo.weight ,
+    allergy: patientInfo.allergy,
+    disease: patientInfo.disease,
+    patientType:patientInfo.patient_type ,
+    address: patientInfo.address,
+    patient_updated_by: currentUser.employee_name,
+    patient_updated_by_emp_id: currentUser.employee_ID,
+  }); 
+  console.log(data)
+
+  const getDisease = async () =>{
+    try{
+     const response = await axios.get('http://localhost:4000/api/v1/receptionist/get-disease');
+     console.log(response);
+     setDisease(response?.data?.data)
+    }
+    catch(error){
+       console.log(error)
+    }
+   
+
+ }
+ useEffect(()=>{
+    getDisease();
+ },[])
+
+ const handleChangeDisease = (newValue, actionMeta) => {
+  
+    setSelectedDisease(newValue);
+    if (actionMeta.action === 'create-option') {
+      // If a new option is created, add it to the list of options
+      const newOption = { value: newValue[newValue.length - 1].value, label: newValue[newValue.length - 1].label };
+      disease.push(newOption);
+    }
+  };
+
+  const handleChange = (e) => {
+    const {name,value} = e.target;
+    setData({
+      ...data,
+      [name] : value
+    })
+  }
+
+  return (
+    <>
+   <Wrapper className="container">
+   <>
+    
+
+      <Modal show={true} onHide={onClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Edit Patient Details</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+
+        <form>
+          <div class="mb-3">
+            <label for="recipient-name" class="col-form-label">Patient Id</label>
+            <input type="text" class="form-control" readOnly value={data.patientId} id="recipient-name"/>
+          </div>
+          <div class="mb-3">
+          <label className="form-label" for="form6Example1">
+                            Patient name
+                          </label>
+                          <input
+                            type="text"
+                            id="form6Example1"
+                            className="form-control"
+                            name="patient_Name"
+                            onChange={handleChange}
+                            value={data.patient_Name}
+                            required
+                          />
+          </div>
+          <div class="mb-3">
+          <label className="form-label" for="form6Example1">
+                            Moblie
+                          </label>
+                          <input
+                            required
+                            type="text"
+                            className="form-control"
+                            value={data.mobile}
+                            name="mobile"
+                            placeholder=""
+                            maxLength={10}
+                            minLength={10}
+                            onChange={handleChange}
+
+                          />
+          </div>
+          <div class="mb-3">
+          <label className="form-label" for="form6Example2">
+                            Email
+                          </label>
+                          <input
+                            type="email"
+                            id="form6Example2"
+                            className="form-control"
+                            name="email"
+                            onChange={handleChange}
+                            value={data.email}
+                          />
+          </div>
+          <div class="mb-3">
+          <label className="form-label" htmlFor="">Gender</label>
+    <select className="form-select" value={data.gender} id="gender" name="gender"  required onChange={handleChange}>
+      <option value="">Select Gender</option>
+      <option value="Male">Male</option>
+      <option value="Female">Female</option>
+      <option value="Other">Other</option>
+     
+    </select>
+          </div>
+          <div class="mb-3">
+          <label className="form-label" for="form6Example1">
+                            Address
+                          </label>
+                          <input
+                            type="text"
+                            id="form6Example1"
+                            className="form-control"
+                            name="address"
+                            onChange={handleChange}
+                            required
+                            value={data.address}
+                          />
+          </div>
+          <div class="mb-3">
+          <label className="form-label" for="form6Example1">
+                           Aadhaar No.
+                          </label>
+                          <input
+                            type="text"
+                            id="form6Example1"
+                            className="form-control"
+                            name="aadhaar_no"
+                            onChange={handleChange}
+                            value={data.aadhaar_no}
+                          
+                          />
+          </div>
+          <div class="mb-3">
+          <label className="form-label" htmlFor="">Contact Person</label>
+    <select className="form-select" id="contact_Person" name="contact_Person" value={data.contact_Person} required onChange={handleChange}>
+      <option value="">Select</option>
+      <option value="Self">Self</option>
+      <option value="Father">Father</option>
+      <option value="Mother">Mother</option>
+      <option value="Husband">Husband</option>
+      <option value="Wife">Wife</option>
+      <option value="Son">Son</option>
+      <option value="Daughter">Daughter</option>
+      <option value="Other">Other</option>
+     
+    </select>
+          </div>
+          <div class="mb-3">
+          <label className="form-label" for="form6Example2">
+                            Cont. Per. Name
+                          </label>
+                          <input
+                            type="text"
+                            id="contact_Person_Name"
+                            name="contact_Person_Name"
+                            className="form-control"
+                            onChange={handleChange}
+                            value={data.contact_Person_Name}
+                            required
+                          />
+          </div>
+          <div class="mb-3">
+          <label className="form-label" htmlFor="">Blood Group</label>
+    <select className="form-select" id="blood_Group" name="blood_Group" onChange={handleChange} value={data.blood_Group}>
+      <option value="">Select</option>
+      <option value="A+">A+</option>
+      <option value="B+">B+</option>
+      <option value="O+">O+</option>
+      <option value="AB+">AB+</option>
+      <option value="A-">A-</option>
+      <option value="B-">B-</option>
+      <option value="O-">O-</option>
+      <option value="AB-">AB-</option>
+     
+    </select>
+          </div>
+          <div class="mb-3">
+          <label
+                            className="form-label"
+                            for="form6Example1"
+                            
+                          >
+                            Date of Birth
+                          </label>
+                          <input
+                            type="date"
+                            id="form6Example1"
+                            className="form-control"
+                            name="dob"
+                            value={data.dob}
+                            onChange={(e) => {
+                              handleChange(e)
+                            }}
+                          />
+          </div>
+          <div class="mb-3">
+          <label className="form-label" for="form6Example2">
+                            Age
+                          </label>
+
+                          <input
+                            type="text"
+                            id="form6Example2"
+                            className="form-control"
+                            name="age"
+                            onChange={handleChange}
+                            value={data.age ? data.age : ""}
+                            required
+                          />
+          </div>
+          <div class="mb-3">
+          <label className="form-label" for="form6Example2">
+                            Weight
+                          </label>
+
+                          <input
+                            type="text"
+                            id="form6Example2"
+                            className="form-control"
+                            name="weight"
+                            onChange={handleChange}
+                            required
+                            value={data.weight}
+                          />
+          </div>
+          <div class="mb-3">
+          <label className="form-label" for="form6Example2">
+                         Have any allergy
+                          </label>
+
+                          <input
+                            type="text"
+                            id="form6Example2"
+                            className="form-control"
+                            name="allergy"
+                            onChange={handleChange}
+                            value={data.allergy}
+                        
+                          />
+          </div>
+          <div class="mb-3">
+          <label className="form-label" for="form6Example2">
+                         Have any disease
+                          </label>
+                          <CreatableSelect
+        id="disease"
+        isMulti
+        onChange={handleChangeDisease}
+        options={disease}
+        value={selectedDisease}
+        inputValue={inputDisease}
+        onInputChange={setInputDisease}
+        placeholder="Select or type to add..."
+      />
+          </div>
+          <div class="mb-3">
+          <label className="form-label" for="form6Example2">
+                         Patient Type
+                          </label>
+
+                          <select className="form-select" id="patientType" name="patientType" value={data.patientType} required onChange={handleChange}>
+      <option value="">Select Patient Type</option>
+      <option value="Genral">General</option>
+      <option value="CGHS(Serving)">CGHS(Serving)</option>
+      <option value="CGHS(Pensioner)">CGHS(Pensioner)</option>
+      <option value="CSMA">CSMA</option>
+  
+     
+    </select>
+          </div>
+         
+        <button type="button" class="btn btn-primary">Edit</button>
+        </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={onClose}>
+            Close
+          </Button>
+  
+        </Modal.Footer>
+      </Modal>
+    </>
+        
+   </Wrapper>
+  
+    </>
+  )
+}
+
+export default EditPatientDetails
+const Wrapper = styled.div`
+
+
+
+`
+const DoctorList = styled.div`
+  position: absolute;
+  z-index: 999; /* Set a high z-index to ensure the list is displayed above other elements */
+  width: 100%;
+  overflow-y: auto;
+  max-height: 400px;
+ 
+  /* Your additional styling for the doctor list */
+`;

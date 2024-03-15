287cb39609d11b6ee4d3ce62b80d824bfdfc6cd1
mohitsahu21 <mohitsahu1993@gmail.com>
2024-03-15 21:25:26 +0530

    update with doctor data.

--- frontend/src/pages/receptionist/DoctorSection.js
+++ frontend/src/pages/receptionist/DoctorSection.js
@@ -1,16 +1,68 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import Sider from '../../components/receptionist/Sider'
 import Header from '../../components/receptionist/Header'
 import { Link } from 'react-router-dom'
 import { Table, Input, Button, Form } from "react-bootstrap";
+import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios'
 
 function DoctorSection() {
+
+  const {refreshTable,currentUser} = useSelector((state) => state.user);
+  const  branch = currentUser.branch_name;
+  const [branchDetail,setBranchDetail] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [rowsPerPage, setRowsPerPage] = useState(10);
 
   const [filteredData, setFilteredData] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [doctors,setDoctors] = useState([]);
+  
+  const [doctorWithLeave,setDoctorWithLeave] = useState([]);
+
+  const getDoctorsWithLeave = async ()=>{
+    try{
+      const response = await axios.get(`http://localhost:4000/api/v1/receptionist/get-doctors-with-leave/${branch}`);
+      setDoctorWithLeave(response?.data?.data)
+    }
+    catch(error){
+      console.log(error)
+    }
+  }
+
+  const getDoctors = async ()=>{
+    try{
+      const response = await axios.get(`http://localhost:4000/api/v1/receptionist/get-doctors/${branch}`);
+      setDoctors(response?.data?.data)
+    }
+    catch(error){
+      console.log(error)
+    }
+  }
+
+  const getBranchDetail = async ()=>{
+    try{
+       const response = await axios.get(`http://localhost:4000/api/v1/receptionist/get-branch-detail/${branch}`)
+       console.log(response)
+       setBranchDetail(response.data.data)
+    }
+    catch(error){
+      console.log(error)
+    }
+  }
+
+  useEffect(()=>{
+     
+     getDoctors();
+     
+     getDoctorsWithLeave();
+     getBranchDetail();
+    
+     
+  },[]);
+
+  console.log(doctorWithLeave)
 
   // Function to convert 24-hour time to AM/PM format
 const convertToAMPM = (time) => {
@@ -28,25 +80,14 @@ const convertToAMPM = (time) => {
 
 
 
-  const Table_data = [
-    { uid :"1", doctor_name:"Dr Umer Qureshi",department:"ortho", mobile: "9806324245", email:"doctor@gmail.com",gender:"Male",address:"Ranital Gate no.4 Jabalpur", morningStartTiming:"10:00" ,morningEndTiming:"14:00",eveningStartTiming:"18:00" ,eveningEndTiming:"21:00",  scheduleBlockDays:"20/02/2024",lunchTime: ""},
-    { uid :"2", doctor_name:"Dr Mohit Sahu",department:"ortho", mobile: "9806324245", email:"doctor@gmail.com",gender:"Male",address:"Ranital Gate no.4 Jabalpur", morningStartTiming:"10:00" ,morningEndTiming:"14:00",eveningStartTiming:"18:00" ,eveningEndTiming:"21:00",  scheduleBlockDays:"20/02/2024",lunchTime: ""},
-    { uid :"3", doctor_name:"Dr Shubham Soni",department:"ortho", mobile: "9806324245", email:"doctor@gmail.com",gender:"Male",address:"Ranital Gate no.4 Jabalpur", morningStartTiming:"10:00" ,morningEndTiming:"14:00",eveningStartTiming:"18:00" ,eveningEndTiming:"21:00",  scheduleBlockDays:"20/02/2024",lunchTime: ""},
-    { uid :"4", doctor_name:"Dr Dev",department:"ortho", mobile: "9806324245", email:"doctor@gmail.com",gender:"Male",address:"Ranital Gate no.4 Jabalpur", morningStartTiming:"10:00" ,morningEndTiming:"14:00",eveningStartTiming:"18:00" ,eveningEndTiming:"21:00",  scheduleBlockDays:"20/02/2024",lunchTime: ""},
-    { uid :"5", doctor_name:"Dr Mahesh Kuldeep",department:"ortho", mobile: "9806324245", email:"doctor@gmail.com",gender:"Male",address:"Ranital Gate no.4 Jabalpur", morningStartTiming:"10:00" ,morningEndTiming:"14:00",eveningStartTiming:"18:00" ,eveningEndTiming:"21:00",  scheduleBlockDays:"20/02/2024",lunchTime: ""},
-   
-    
-
-  ];
-
     // Searching function
     const handleSearch = (event) => {
       const searchTerm = event.target.value.toLowerCase();
       setSearchTerm(searchTerm);
       setCurrentPage(1); // Reset to the first page when searching
   
-      const filteredResults = Table_data.filter((row) =>
-        row.doctor_name.toLowerCase().includes(searchTerm)
+      const filteredResults = doctors.filter((row) =>
+        row.employee_name.toLowerCase().includes(searchTerm)
       );
   
       setFilteredData(filteredResults);
@@ -62,10 +103,10 @@ const convertToAMPM = (time) => {
   // Pagination functions
   const indexOfLastRow = currentPage * rowsPerPage;
   const indexOfFirstRow = indexOfLastRow - rowsPerPage;
-  const currentRows = searchTerm ? filteredData.slice(indexOfFirstRow, indexOfLastRow) : Table_data.slice(indexOfFirstRow, indexOfLastRow);
+  const currentRows = searchTerm ? filteredData.slice(indexOfFirstRow, indexOfLastRow) : doctors.slice(indexOfFirstRow, indexOfLastRow);
 
 
-  const totalPages = Math.ceil(Table_data.length / rowsPerPage);
+  const totalPages = Math.ceil(doctors.length / rowsPerPage);
 
   const paginate = (pageNumber) => {
     if (pageNumber > 0 && pageNumber <= totalPages) {
@@ -74,7 +115,7 @@ const convertToAMPM = (time) => {
   };
 
   const pageNumbers = [];
-  for (let i = 1; i <= Math.ceil(Table_data.length / rowsPerPage); i++) {
+  for (let i = 1; i <= Math.ceil(doctors.length / rowsPerPage); i++) {
     pageNumbers.push(i);
   }
 
@@ -141,106 +182,128 @@ const convertToAMPM = (time) => {
   
   return (
     <Wrapper>
-        <Header/>
-           <div className="row flex-nowrap">
-    <div className="col-lg-1 col-1 " id='sider'>
-   <Sider/>
-   </div>
-   <div className="col-lg-11">
-    <div className="row" id='row1'>
-      <div className="col-lg-11">
-       
-      <div className="input-group mt-4 ">
-      <Form.Group
-                      controlId="rowsPerPageSelect"
-                      style={{ display: "flex" }}
-                    >
-                      <Form.Label className="d-flex align-items-center">
-                        Rows Per Page :{" "}
-                      </Form.Label>
-                      <Form.Control
-                        as="select"
-                        value={rowsPerPage}
-                        className="m-2"
-                        style={{ width: "auto" }}
-                        onChange={handleRowsPerPageChange}
-                      >
-                        
-                        <option value={10}>10</option>
-                        <option value={25}>25</option>
-                        <option value={50}>50</option>
-                        {/* Add more options as needed */}
-                      </Form.Control>
-                    </Form.Group>
-  <div className="form-outline  d-flex align-items-center">
-    <input type="search" id="form1" className="form-control" placeholder='Search here' onChange={handleSearch}
-        value={searchTerm} />
-    
-  </div>
-  
-  
+      <Header />
+      <div className="row flex-nowrap">
+        <div className="col-lg-1 col-1 " id="sider">
+          <Sider />
+        </div>
+        <div className="col-lg-11">
+          <div className="row" id="row1">
+            <div className="col-lg-11">
+              <div className="input-group mt-4 ">
+                <Form.Group
+                  controlId="rowsPerPageSelect"
+                  style={{ display: "flex" }}
+                >
+                  <Form.Label className="d-flex align-items-center">
+                    Rows Per Page :{" "}
+                  </Form.Label>
+                  <Form.Control
+                    as="select"
+                    value={rowsPerPage}
+                    className="m-2"
+                    style={{ width: "auto" }}
+                    onChange={handleRowsPerPageChange}
+                  >
+                    <option value={10}>10</option>
+                    <option value={25}>25</option>
+                    <option value={50}>50</option>
+                    {/* Add more options as needed */}
+                  </Form.Control>
+                </Form.Group>
+                <div className="form-outline  d-flex align-items-center">
+                  <input
+                    type="search"
+                    id="form1"
+                    className="form-control"
+                    placeholder="Search here"
+                    onChange={handleSearch}
+                    value={searchTerm}
+                  />
+                </div>
+              </div>
+              <div className="widget-area-2 proclinic-box-shadow" id="tableres">
+                <div className="table-responsive">
+                  <table className="table table-bordered table-striped">
+                    <thead>
+                      <tr>
+                        <th>Id</th>
+                        <th>Doctor Name</th>
+                        <th>Mobile</th>
+                        <th>Email</th>
+                        <th>Gender</th>
+                        <th>Address</th>
+                        <th>Shift</th>
+                        <th>Leave Dates</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {currentRows.map((data, index) => (
+                        <tr key={index}>
+                          <td>{data.employee_ID}</td>
+                          <td> {data.employee_name}</td>
+                          <td>{data.employee_mobile}</td>
+                          <td>{data.employee_email}</td>
 
-
- 
-</div>
-          <div className="widget-area-2 proclinic-box-shadow" id='tableres'>
-                   
-                    <div className="table-responsive">
-                      <table className="table table-bordered table-striped">
-                        <thead>
-                          <tr>
-                            <th>Id</th>
-                            <th>Doctor Name</th>
-                            <th>Mobile</th>
-                            <th>Email</th>
-                            <th>Gender</th>
-                            <th>Address</th>
-                            <th>Shift</th>
-                            <th>Appointment</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          {currentRows.map((data,index) => (
-                            
-                                  <tr key={index}>
-                                  <td>{data.uid}</td>
-                                <td> {data.doctor_name}</td>
-                                  <td>{data.mobile}</td>
-                                  <td>{data.email}</td>
-      
-                                  <td>{data.gender}</td>
-                                  <td>
-                                   {data.address}
-                                  </td>
-                                  <td>Morning : <span> {convertToAMPM(data.morningStartTiming) } to {convertToAMPM(data.morningEndTiming) }</span> 
-                                  <span className='d-block'>Evening : {convertToAMPM(data.eveningStartTiming)} to {convertToAMPM(data.eveningEndTiming)}</span>   </td>
-                                  <td><Link to='/doctor_profile'>View Details</Link></td>
-                                </tr>
-                          ))}
-                      
-                         
-                         
-                         
-                         
-                          
-                        </tbody>
-                      </table>
-                    </div>
-                    <div className="container mt-3 mb-3">
+                          <td>{data.gender}</td>
+                          <td>{data.address}</td>
+                          <td>
+                            Morning :{" "}
+                            <span>
+                              {" "}
+                              {convertToAMPM(
+                                data.morning_shift_start_time
+                              )} to {convertToAMPM(data.morning_shift_end_time)}
+                            </span>
+                            <span className="d-block">
+                              Evening :{" "}
+                              {convertToAMPM(data.evening_shift_start_time)} to{" "}
+                              {convertToAMPM(data.evening_shift_end_time)}
+                            </span>{" "}
+                          </td>
+                          {/* <td><Link to='/doctor_profile'>View Details</Link></td> */}
+                          <td>
+                          {doctorWithLeave
+    .filter((doctor) => doctor.employee_ID === data.employee_ID)
+    .map((doctor) => 
+      doctor.leave_dates.split(",")
+        .filter((leaveDate) => new Date(leaveDate) >= new Date()).join(", ") // Filter out leave dates less than today's date
+        
+    )}
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+                <div className="container mt-3 mb-3">
                   <div className="row">
-                    <div className="col-lg-8 col-md-8 col-sm-12 col-12">  <h4
-                          style={{
-                            color: "black",
-                            marginLeft: "5px",
-                            marginRight: "5px",
-                            fontSize: "1.1rem"
-                          }}
-                          
-                        >
-                         {/* Showing Page {currentPage} of {totalPages} from {data?.length} entries */}
-                       {searchTerm ?<> Showing Page {currentPage} of {totalPages} from {filteredData?.length} entries (filtered from {Table_data?.length} total entries) </> : <>Showing Page {currentPage} of {totalPages} from {Table_data?.length} entries</> }  
-
-                        </h4></div>
+                    <div className="col-lg-8 col-md-8 col-sm-12 col-12">
+                      {" "}
+                      <h4
+                        style={{
+                          color: "black",
+                          marginLeft: "5px",
+                          marginRight: "5px",
+                          fontSize: "1.1rem",
+                        }}
+                      >
+                        {/* Showing Page {currentPage} of {totalPages} from {data?.length} entries */}
+                        {searchTerm ? (
+                          <>
+                            {" "}
+                            Showing Page {currentPage} of {totalPages} from{" "}
+                            {filteredData?.length} entries (filtered from{" "}
+                            {doctors?.length} total entries){" "}
+                          </>
+                        ) : (
+                          <>
+                            Showing Page {currentPage} of {totalPages} from{" "}
+                            {doctors?.length} entries
+                          </>
+                        )}
+                      </h4>
+                    </div>
                     <div className="col-lg-3 col-md-3 col-sm-12 col-12">
                       <div className="d-flex justify-content-evenly">
                         <Button
@@ -251,10 +314,10 @@ const convertToAMPM = (time) => {
                           Previous
                         </Button>
                         {renderPageNumbers}
-                      
+
                         <Button
                           onClick={() => paginate(currentPage + 1)}
-                          disabled={indexOfLastRow >= Table_data.length}
+                          disabled={indexOfLastRow >= doctors.length}
                           variant="success"
                         >
                           Next
@@ -263,18 +326,13 @@ const convertToAMPM = (time) => {
                     </div>
                   </div>
                 </div>
-                  </div>
-      </div>
-  
-   
-    
+              </div>
+            </div>
           </div>
-   </div>
-   </div>
-        
-     
+        </div>
+      </div>
     </Wrapper>
-  )
+  );
 }
 
 export default DoctorSection

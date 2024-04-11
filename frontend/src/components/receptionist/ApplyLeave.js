5bde5a1529648a40d8abc618f0976174efde570f
mohitsahu21 <mohitsahu1993@gmail.com>
2024-04-11 21:07:11 +0530

    add the page for leave and attendance

---
+++ frontend/src/components/receptionist/ApplyLeave.js
@@ -0,0 +1,221 @@
+
+
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import { useDispatch ,useSelector} from "react-redux";
+import cogoToast from 'cogo-toast';
+import axios from "axios";
+import moment from "moment";
+import { toggleTableRefresh } from '../../redux/user/userSlice';
+
+function ApplyLeave() {
+    const {refreshTable,currentUser} = useSelector((state) => state.user);
+    const dispatch = useDispatch();
+  const  branch = currentUser.branch_name;
+  const employeeName = currentUser.employee_name;
+  const employeeId = currentUser.employee_ID;
+  const [show, setShow] = useState(false);
+  const [leavesData,setLeaveData] = useState([]);
+  const [selectedDates, setSelectedDates] = useState([]);
+  const [formData,setFormData] = useState({
+    employee_ID : employeeId,
+    employee_name : employeeName,
+    branch_name : branch,
+    leave_dates : selectedDates,
+    leave_reason : "",
+    leave_status : "pending"
+  
+  });
+
+  const getLeaves = async () => {
+     
+    try{
+        const response = await axios.get(`http://localhost:4000/api/v1/receptionist/get-leaves/${branch}/${employeeId}`);
+        setLeaveData(response?.data?.data)
+      }
+      catch(error){
+        console.log(error)
+      }
+  }
+
+  useEffect(()=>{
+    getLeaves();
+  },[])
+
+
+ console.log(leavesData)
+  // Function to format date to "YYYY-MM-DD"
+  const formatDate = (date) => {
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  };
+
+  const handleDateClick = (date) => {
+    const formattedDate = formatDate(date);
+    const dateIndex = selectedDates.indexOf(formattedDate);
+
+    // If the date is already in the array, remove it
+    if (dateIndex !== -1) {
+      setSelectedDates((prevDates) =>
+        prevDates.filter((_, index) => index !== dateIndex)
+      );
+    } else {
+      // Otherwise, add the date to the array
+      setSelectedDates((prevDates) => [...prevDates, formattedDate]);
+    }
+  };
+  const handleChange = (e) =>{
+    const {value,name} = e.target;
+    setFormData((pre) =>({
+        ...pre,
+        [name]:value}))
+ 
+  }
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+
+    // const existingLeave = 
+    //         selectedDates.every(
+    //             (date) => leavesData.some((leave) => leave.leave_dates.includes(date))
+    //         )
+      if(selectedDates.length === 0){
+        cogoToast.error("Please select a date")
+        return;
+      }
+
+      
+    //   if(existingLeave){
+        
+    //     cogoToast.error("You have already apply leave for this date")
+    //     return;
+    //   }
+     // Check if any of the selected dates overlap with existing leave data
+     const hasOverlap = selectedDates.some(date =>
+        leavesData.some(leave => leave.leave_dates.includes(date))
+    );
+
+    if (hasOverlap) {
+        // If any of the selected dates overlap with existing leave data, show an error and return
+        cogoToast.error("You have already applied for leave on one or more of these dates.");
+        return;
+    }
+
+       
+      else{
+        const updatedFormData = {
+            ...formData,
+            leave_dates: selectedDates.toString(),
+    
+        }
+        try {
+            const response = await axios.post('http://localhost:4000/api/v1/receptionist/apply-leave', updatedFormData);
+            if(response.data.success){
+                cogoToast.success(response?.data?.message);
+                setSelectedDates([]);
+                setFormData((prev) => ({
+                    ...prev,
+                    leave_reason : ""
+                }))
+                getLeaves();
+                dispatch(toggleTableRefresh())
+                handleClose();
+    
+            }
+            else{
+                cogoToast.error(response?.data?.message);
+            }
+        } catch (error) {
+            console.log(error)
+          cogoToast.error(error?.response?.data?.message);
+        }
+      }
+    
+  }
+
+console.log(formData)
+  const handleClose = () =>{ 
+    setSelectedDates([]);
+    setShow(false)};
+  const handleShow = () => setShow(true);
+
+  return (
+    <Wrapper className="container">
+      <Button variant="info" onClick={handleShow} id="b1">
+        Apply Leave
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Apply for Leave</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+              <label htmlFor="leaveReason" className="form-label">
+                Leave Reason
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="leaveReason"
+                placeholder="Leave reason"
+                name="leave_reason"
+                onChange={handleChange}
+                required
+                value={formData.leave_reason}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="calendar" className="form-label">
+                Leave Dates
+              </label>
+              {/* Calendar component */}
+              <Calendar
+                onChange={handleDateClick}
+                value={null}
+                calendarType="gregory"
+                selectRange={false}
+              />
+            </div>
+
+            {/* Display the selected dates */}
+            <div>
+              <strong>Selected Dates:</strong>
+              <ul>
+                {selectedDates.map((date) => (
+                  <li key={date}>{ moment(date, 'YYYY-MM-DD').format('DD-MM-YYYY')}</li>
+                ))}
+              </ul>
+            </div>
+
+            <div>
+              <button type="submit" className="btn btn-success">
+                Apply
+              </button>
+            </div>
+          </form>
+          <div className="widget-area-2 proclinic-box-shadow" id="tableres"></div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Cancel
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </Wrapper>
+  );
+}
+
+export default ApplyLeave;
+
+const Wrapper = styled.div`
+  #button {
+    margin: 2rem;
+  }
+`;
---
+++ frontend/src/pages/receptionist/AttendanceLeave.jsx
@@ -0,0 +1,395 @@
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import styled from "styled-components";
+
+import { IoMdArrowRoundBack } from "react-icons/io";
+import { useDispatch ,useSelector} from "react-redux";
+
+import axios from "axios";
+import moment from "moment";
+
+import cogoToast from "cogo-toast";
+
+// import BranchDetails from "../components/BranchDetails";
+// import ApplyLeave from "../components/btModal/ApplyLeave";
+import Header from "../../components/receptionist/Header";
+import Sider from "../../components/receptionist/Sider";
+import { useNavigate } from 'react-router-dom';
+import ApplyLeave from "../../components/receptionist/ApplyLeave";
+
+const AttendanceLeave = () => {
+    const navigate = useNavigate();
+    const {refreshTable,currentUser} = useSelector((state) => state.user);
+    const  branch = currentUser.branch_name;
+    const employeeName = currentUser.employee_name;
+    const employeeId = currentUser.employee_ID;
+  const [attendRepo, setAttendRepo] = useState([]);
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const [daysInMonth, setDaysInMonth] = useState([]);
+  const [monthName, setMonthName] = useState("");
+  const [formattedDate, setFormattedDate] = useState("");
+  const [fromDate, setFromDate] = useState("");
+  const [toDate, setToDate] = useState("");
+  const [leavesData,setLeaveData] = useState([]);
+
+  const getLeaves = async () => {
+     
+    try{
+        const response = await axios.get(`http://localhost:4000/api/v1/receptionist/get-leaves/${branch}/${employeeId}`);
+        setLeaveData(response?.data?.data)
+      }
+      catch(error){
+        console.log(error)
+      }
+  }
+
+  useEffect(()=>{
+    getLeaves();
+  },[refreshTable])
+
+
+
+
+
+
+  const generateDaysInMonth = (fromDate, toDate) => {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = currentDate.getMonth();
+    const daysInMonth = new Date(year, month + 1, 0).getDate();
+    const daysArray = Array.from({ length: daysInMonth }, (_, i) => {
+      const dayNumber = i + 1;
+      return dayNumber < 10 ? `0${dayNumber}` : `${dayNumber}`;
+    });
+    const monthName = new Intl.DateTimeFormat("en-US", {
+      month: "long",
+    }).format(currentDate);
+
+    const formattedDate = currentDate.toISOString().split("T")[0];
+    setFormattedDate(formattedDate);
+
+    if (fromDate && toDate) {
+      const filteredDaysArray = daysArray.filter((day) => {
+        const date = new Date(year, month, parseInt(day, 10));
+        return date >= new Date(fromDate) && date <= new Date(toDate);
+      });
+      setDaysInMonth(filteredDaysArray);
+    } else {
+      setDaysInMonth(daysArray);
+    }
+
+    setMonthName(monthName);
+  };
+
+  useEffect(() => {
+    generateDaysInMonth(fromDate, toDate);
+  }, [fromDate, toDate]);
+
+  useEffect(() => {
+    generateDaysInMonth();
+  }, [currentDate]);
+
+  console.log(attendRepo);
+
+  console.log(formattedDate.slice(0, 7));
+  console.log(attendRepo[0]?.date);
+  console.log(`${formattedDate.slice(0, 7)}-${daysInMonth[0]}`);
+  const filter = daysInMonth.map((day) => {
+    return attendRepo.find(
+      (item) =>
+        item.date.split("T")[0] === `${formattedDate.slice(0, 7)}-${day}`
+    );
+  });
+
+  console.log(filter[5]?.login);
+  console.log(attendRepo[0]?.date.split("T")[0] >= fromDate);
+  console.log(toDate);
+  const fitlerByDuration = attendRepo?.filter((item) => {
+    return (
+      item.date.split("T")[0] >= fromDate && item.date.split("T")[0] <= toDate
+    );
+  });
+
+  console.log(fitlerByDuration);
+
+  const goBack = (event) => {
+    event.preventDefault(); // Prevent default action to avoid page refresh
+    navigate(-1); // This goes back to the previous page
+};
+
+
+// Helper function to format a single date string in the desired format
+const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    return date.toLocaleDateString("en-GB", options);
+};
+  return (
+    <>
+      <Container>
+      <div className='header'>
+          <Header/>
+          </div>
+    
+    <div className="row mrgnzero">
+      <div className="col-lg-1 col-md-1 col-1" id="sider">
+        <Sider />
+      </div>
+              <div className="col-lg-11 col-md-11 col-11" id="set">
+                <div className="container-fluid mt-3">
+                  <div className="">
+                    {/* <BranchDetails /> */}
+                  </div>
+                </div>
+
+                <div className="container-fluid mt-3">
+                  <button className="btn btn-success" onClick={goBack}>
+                    <IoMdArrowRoundBack /> Back
+                  </button>
+                  <div className="container-fluid">
+                    <div className="row mt-3">
+                      <div className="col-12">
+                        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                          <div class="container d-flex justify-content-center">
+                            <h2 className="">Attendance and Leave Details</h2>
+                          </div>
+                        </nav>
+                      </div>
+                      <div className="container-fluid">
+                        <div className="container-fluid">
+                          <div className="d-flex justify-content-between mb-2 mt-4">
+                            <form>
+                              <div className="d-flex justify-content-between">
+                                <div>
+                                  <input
+                                    type="date"
+                                    name=""
+                                    id=""
+                                    className="p-2 rounded"
+                                    onChange={(e) =>
+                                      setFromDate(e.target.value)
+                                    }
+                                  />
+                                </div>
+                                <div className="mx-2">To</div>
+                                <div>
+                                  <input
+                                    type="date"
+                                    name=""
+                                    id=""
+                                    className="p-2 rounded"
+                                    onChange={(e) => setToDate(e.target.value)}
+                                  />
+                                </div>
+                                <button
+                                  className="btn btn-warning mx-2"
+                                  type="submit"
+                                >
+                                  Download Report
+                                </button>
+                              </div>
+                            </form>
+                            <div>
+                              <ApplyLeave />
+                            </div>
+                          </div>
+                          <div class="table-responsive">
+                            <table class="table table-bordered">
+                              <thead className="table-head">
+                                <tr>
+                                  <th>EMP ID</th>
+                                  <th>Employee Name</th>
+                                  <th>Dessignation</th>
+
+                                  {daysInMonth.map((day) => (
+                                    <>
+                                      <th key={day}>
+                                        {day} {monthName}
+                                      </th>
+                                    </>
+                                  ))}
+                                </tr>
+                              </thead>
+                              <tbody>
+                                {attendRepo.map((attendItem) => (
+                                  <tr
+                                    className="table-row"
+                                    key={attendItem.employee_ID}
+                                  >
+                                    <td>{attendItem.employee_ID}</td>
+                                    <td>{attendItem.emp_name}</td>
+                                    <td>{attendItem.employee_designation}</td>
+                                    {daysInMonth.map((day) => {
+                                      const attendanceForDay = attendRepo.find(
+                                        (repoItem) =>
+                                          repoItem.date.split("T")[0] ===
+                                            `${formattedDate.slice(
+                                              0,
+                                              7
+                                            )}-${day}` &&
+                                          repoItem.employee_ID ===
+                                            attendItem.employee_ID
+                                      );
+
+                                      return (
+                                        <td key={day}>
+                                          {attendanceForDay ? (
+                                            <span
+                                              className={
+                                                attendanceForDay.status ===
+                                                "approved"
+                                                  ? "attend-approve"
+                                                  : "attend-reject"
+                                              }
+                                            >
+                                              Login - $
+                                              {attendanceForDay.login
+                                                .split(".")[0]
+                                                .slice(0, 5)}{" "}
+                                              & Logout - $
+                                              {attendanceForDay.logout
+                                                .split(".")[0]
+                                                .slice(0, 5)}
+                                            </span>
+                                          ) : (
+                                            "-"
+                                          )}
+                                        </td>
+                                      );
+                                    })}
+                                  </tr>
+                                ))}
+                              </tbody>
+                            </table>
+                          </div>
+                          
+                          <div className="mt-5">
+                            <div className="mt-5 text-center">
+                             <h4>Leave details</h4>
+                            </div>
+                          <div class="table-responsive">
+                            <table class="table table-bordered">
+                              <thead className="table-head">
+                                <tr>
+                                  <th>Sr. no.</th>
+                                  <th>Leave Date</th>
+                                  <th>Leave Reason</th>
+                                  <th>Leave Status</th>
+                                </tr>
+                              </thead>
+                              <tbody>
+                                {leavesData?.map((item,index) => {
+
+                                    // Split the leave_dates string into an array of date strings
+                            const leaveDatesArray = item?.leave_dates.split(",");
+
+                            // Format each date in the array using the formatDate function
+                            const formattedLeaveDates = leaveDatesArray
+                                .map((dateString) => formatDate(dateString))
+                                .join(", "); // Join the formatted dates back into a single string
+                                    return(
+                                        <tr
+                                        className="table-row"
+                                        key={item.id}
+                                      >
+                                        <td>{index+1}</td>
+                                        <td>{formattedLeaveDates}</td>
+                                        <td>{item.leave_reason}</td>
+                                        <td>{item.leave_status}</td>
+                                       
+                                      </tr>
+                                    )
+                                  
+                        })}
+                              </tbody>
+                            </table>
+                          </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+         
+      </Container>
+    </>
+  );
+};
+
+export default AttendanceLeave;
+const Container = styled.div`
+  .table {
+    overflow-x: auto;
+    th {
+      background-color: teal;
+      color: white;
+      min-width: 7rem;
+    }
+    td {
+      font-weight: bold;
+      min-width: 7rem;
+    }
+  }
+  .table::-webkit-scrollbar {
+    width: 0;
+  }
+
+  .select-style {
+    border: none;
+    background-color: #22a6b3;
+    font-weight: bold;
+    color: white;
+  }
+
+  .attend-reject {
+    color: red;
+  }
+
+  .attend-approve {
+    color: green;
+  }
+  .mrgnzero {
+    margin-right: 0rem;
+  }
+  #sider {
+    padding-top: 60px; /* Height of header */
+  min-height: 100vh;
+  position: fixed;
+  @media screen and (max-width: 768px) {
+   height: 68rem;
+  }
+  @media screen and (min-width: 768px) and (max-width: 1020px) {
+   height: 58rem;
+  }
+  }
+  .header{
+  position: fixed;
+  min-width: 100%;
+  z-index: 100;
+}
+#set{
+
+margin-left: -4.5rem;
+padding-left: 150px; /* Width of sidebar */
+padding-top: 90px; /* Height of header */
+flex-grow: 1;
+overflow-y: auto;
+
+@media screen and (max-width: 768px) {
+  margin-left: -2rem;
+}
+@media screen and (min-width: 768px) and (max-width : 1020px) {
+  margin-left: -2rem;
+}
+@media screen and (min-width: 1020px) and (max-width: 1500px) {
+  margin-left: -2rem;
+  
+}
+@media screen and (min-width: 1500px) and (max-width: 2000px) {
+  margin-left: -1.9rem;
+  
+}
+}
+`;
